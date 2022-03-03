fb5037e7d2cebac45b3e40beafbc4b1845cb6296
Brandon Allen <ballen@symbotic.com>
2022-03-03 16:14:32 -0500

    bugfix ?

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -16,6 +16,3 @@ if __name__ == '__main__':
     if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
         cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
         remove_file(cli_file)
-
-    if 'Proprietary' == '{{ cookiecutter.license }}':
-        remove_file('LICENSE')
